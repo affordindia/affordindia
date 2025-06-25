a28dde8e0361928cf315e846a98195a7aae9d0f1
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-06-25 19:56:59 +0530

    bug: fix imports in user.routes

--- admin/backend/routes/user.routes.js
+++ admin/backend/routes/user.routes.js
@@ -1,15 +1,12 @@
 import express from "express";
-import {
-    getAllUsersController,
-    getUserByIdController,
-} from "../controllers/user.controller.js";
+import { getAllUsers, getUserById } from "../controllers/user.controller.js";
 import adminAuth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.use(adminAuth);
 
-router.get("/", getAllUsersController);
-router.get("/:id", getUserByIdController);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
 
 export default router;
