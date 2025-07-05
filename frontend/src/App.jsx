d10ca69a6aca047f80da2e73ea7bdda5338a6092
affordindia <techvcrafthem@gmail.com>
2025-07-05 10:01:18 +0530

    Updated frontend code with OTP login system

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom"; 
+import Navbar from "./components/Navbar";
+import SidebarLayout from "./components/SidebarLayout";
+import AddItems from "./pages/Add";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import Banner from "./pages/Banner";
+import Analytics from "./pages/Analytics";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import OtpLogin from  "./pages/OtpLogin";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SidebarLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="add" element={<AddItems />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="banners" element={<Banner />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="otplogin" element={<OtpLogin />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+    <h1>Banner my mvhfcvjfkb</h1>
+    </>
+  )
+}
+
+export default Banner
