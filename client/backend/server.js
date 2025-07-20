92adde2eebf969637f4111c8ea7a73492c3c6009
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-07-20 13:19:17 +0530

    CORS

--- client/backend/server.js
+++ client/backend/server.js
@@ -20,7 +20,15 @@ import errorHandler from "./middlewares/error.middleware.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5174",
+            "https://client-frontend-chi.vercel.app/",
+        ],
+        credentials: true, // if you use cookies/auth
+    })
+);
 app.use(express.json());
 
 // Connect to DB
