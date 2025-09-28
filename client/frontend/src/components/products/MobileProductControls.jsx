3d358bdfba46a5ab69eb90b6e2b34d39da0283ea
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-09-28 22:45:42 +0530

    format

--- client/frontend/src/components/products/MobileProductControls.jsx
+++ client/frontend/src/components/products/MobileProductControls.jsx
@@ -86,7 +86,9 @@ const MobileProductControls = ({
             {/* Filter Panel */}
             <div
                 className={`fixed left-0 right-0 bottom-0 z-40 flex md:hidden transition-opacity duration-300 ease-in-out ${
-                    showFilterPanel ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                    showFilterPanel
+                        ? "opacity-100 pointer-events-auto"
+                        : "opacity-0 pointer-events-none"
                 }`}
                 style={{
                     top: `${navbarHeight}px`,
